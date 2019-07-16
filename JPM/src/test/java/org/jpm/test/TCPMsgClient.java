3402302c979da8320c4285ac699f08eb77724319
jackwenttohill <32959449+jackwenttohill@users.noreply.github.com>
2019-07-16 11:20:25 +0530

    Update TCPMsgClient.java

--- JPM/src/test/java/org/jpm/test/TCPMsgClient.java
+++ JPM/src/test/java/org/jpm/test/TCPMsgClient.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 import org.jpm.util.PropertyReader;
 
-/**
+/** test
  * @author Ashok Das
  * @since 21-10-2017
  * @see <a href="mailto:jackwenttohill@yahoo.co.uk?Subject=Hello%20World">Hello World</a>
